ac30888fd4ac58d947caced1e9f02fe298023fb2
diana <dianakieru1@gmil.com>
2022-04-18 09:38:01 +0300

    added repo.ts

--- src/app/repo/repo.component.ts
+++ src/app/repo/repo.component.ts
@@ -1,4 +1,7 @@
+import { GitSearchService } from './../../gitSearch-service/git-search.service';
+import { User } from './../../user-class/user';
 import { Component, OnInit } from '@angular/core';
+import { Repository } from 'src/app/repository-class/repository';
 
 @Component({
   selector: 'app-repo',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./repo.component.css']
 })
 export class RepoComponent implements OnInit {
+  user!:User
+  repository!:Repository | any
 
-  constructor() { }
+  constructor(private service:GitSearchService) { 
+    this.repository = new Repository('')
+  }
+  findingUser(user:any){
+    this.service.findingUser(user).then((success)=>{
+      this.user = this.service.user
+    })
+    this.service.findRepos(user).then((success)=>{
+      this.repository = this.service.repository
+    })
+  }
+  
 
   ngOnInit(): void {
+    this.findingUser('Samuel-Muriuki')
+
   }
 
 }
