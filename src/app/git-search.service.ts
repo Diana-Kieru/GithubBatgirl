38b7d0965c5dc55e5d14470c6361ac8d22a25ab0
diana <dianakieru1@gmil.com>
2022-04-19 18:26:53 +0300

    added environment

--- src/app/git-search.service.ts
+++ src/app/git-search.service.ts
@@ -2,9 +2,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-import { environment } from 'src/environments/environment';
+// import { environment } from 'src/environments/environment';
 import { User } from './user';
 import { Repository } from './repository';
+import { environment } from 'src/environments/environment.prod';
 
 
 @Injectable({
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
+  
+  base_Url: "https://api.github.com/users/",
+  api_Key: "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",
   production: true
+  
 };
